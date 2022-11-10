a1b9f2cf451c738bbffa1e23f510c5f518813ca7
Enes Yıldız <enesyld61@gmail.com>
2022-11-10 08:20:32 +0300

    Singleplayer demo-2

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,21 +1,45 @@
 /* eslint-disable jsx-a11y/alt-text */
-import menu from "../images/menu.png";
 import "../MainMenu.css";
-import { Button } from "react-bootstrap";
+import { Button, Col, Row, Carousel, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Rock from "../images/rock.png";
+import Paper from "../images/paper.png";
+import Scissors from "../images/scissors.png";
 export const MainMenu = () => {
   return (
     <div>
-      <h2>Main Menü</h2>
-      <img className="imgMainMenu" src={menu} />
-      <br></br>
-      <br></br>
-      <Link to="/singleplayer">
-        <Button>Singleplayer</Button>
-      </Link>{" "}
-      <Link to="/multiplayer">
-        <Button>Multiplayer</Button>
-      </Link>
+      <h1>Rock-Paper-Scissors</h1>
+      <Container>
+        <Row>
+          <Col xs="6">
+            <Carousel id="abc">
+              <Carousel.Item interval={500}>
+                <img className="carouselImages" src={Rock} />
+              </Carousel.Item>
+              <Carousel.Item interval={500}>
+                <img className="carouselImages" src={Paper} />
+              </Carousel.Item>
+              <Carousel.Item interval={500}>
+                <img className="carouselImages" src={Scissors} />
+              </Carousel.Item>
+            </Carousel>
+          </Col>
+          <Col xs="3">
+            <Link to="/singleplayer">
+              <Button variant="outline-primary" className="btnn">
+                Singleplayer
+              </Button>
+            </Link>{" "}
+          </Col>
+          <Col xs="3">
+            <Link to="/multiplayer">
+              <Button variant="outline-primary" className="btnn">
+                Multiplayer
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
--- src/components/Singleplayer.jsx
+++ src/components/Singleplayer.jsx
@@ -1,30 +1,27 @@
 /* eslint-disable jsx-a11y/alt-text */
 import "../Singleplayer.css";
-import { useState } from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { React, useEffect, useState } from "react";
+import { Button, Col, Container, Row, Form } from "react-bootstrap";
 import Rock from "../images/rock.png";
 import Paper from "../images/paper.png";
 import Scissors from "../images/scissors.png";
+import { Link } from "react-router-dom";
 
 export const Singleplayer = () => {
-  const list = [
-    "btnRock",
-    "btnPaper",
-    "btnScissors",
-    "pcBtn0",
-    "pcBtn1",
-    "pcBtn2",
-  ];
-  const [score, setScore] = useState(0);
   const [pWin, setWin] = useState(0);
   const [pLose, setLose] = useState(0);
   const [pDraw, setDraw] = useState(0);
+  const [maxRound, setMaxRound] = useState(0);
+  const [currentRound, setCurrentRound] = useState(1);
+  const [disable, setDisable] = useState(true);
+  const [selectedBtns, setSelectedBtns] = useState([]);
+  const [result, setResult] = useState("");
+  const [final, setFinal] = useState(false);
 
   const win = (btn, pcBtn) => {
     btn.classList.add("green");
     pcBtn.classList.add("red");
     setWin((prevState) => prevState + 1);
-    setScore((prevState) => prevState + 1);
   };
   const draw = (btn, pcBtn) => {
     btn.classList.add("gray");
@@ -35,7 +32,6 @@ export const Singleplayer = () => {
     btn.classList.add("red");
     pcBtn.classList.add("green");
     setLose((prevState) => prevState + 1);
-    setScore((prevState) => prevState - 1);
   };
 
   const play = (btn, int) => {
@@ -67,83 +63,188 @@ export const Singleplayer = () => {
       }
     }
     document.getElementById("cont").classList.add("wait");
-  };
-
-  const devam = () => {
-    document.getElementById("cont").classList.remove("wait");
-    for (let btn of list) {
-      const doc = document.getElementById(btn).classList;
-      doc.remove("red");
-      doc.remove("green");
-      doc.remove("gray");
+    setCurrentRound((prevState) => prevState + 1);
+    // eslint-disable-next-line eqeqeq
+    if (currentRound == maxRound) {
+      setSelectedBtns([btn, pcBtn]);
+      setFinal(true);
+      setTimeout(finish, 1000);
+    } else {
+      setTimeout(nextRound.bind(null, [btn, pcBtn]), 1000);
     }
   };
+
+  const nextRound = (btns) => {
+    document.getElementById("cont").classList.remove("wait");
+    for (let btn of btns) {
+      btn.classList.remove("red");
+      btn.classList.remove("gray");
+      btn.classList.remove("green");
+    }
+  };
+  const finish = () => {
+    const resultCont = document.getElementById("resultCont");
+    resultCont.classList.remove("close");
+    resultCont.classList.add("open");
+  };
+
+  const start = () => {
+    const playCont = document.getElementById("playCont");
+    playCont.classList.remove("close");
+    playCont.classList.add("open");
+    const chooseCont = document.getElementById("chooseCont");
+    chooseCont.classList.remove("open");
+    chooseCont.classList.add("closing");
+    setTimeout(() => {
+      chooseCont.classList.remove("closing");
+      chooseCont.classList.add("close");
+    }, 300);
+  };
+
+  const clearHistory = () => {
+    setFinal(false);
+    setWin(0);
+    setLose(0);
+    setDraw(0);
+    setCurrentRound(1);
+    for (let btn of selectedBtns) {
+      btn.classList.remove("red");
+      btn.classList.remove("gray");
+      btn.classList.remove("green");
+    }
+  };
+
+  const radioChange = (label) => {
+    setMaxRound(label);
+    setDisable(false);
+  };
+
+  const playAgain = () => {
+    const resultCont = document.getElementById("resultCont");
+    resultCont.classList.remove("open");
+    resultCont.classList.add("closing");
+    setTimeout(() => {
+      resultCont.classList.remove("closing");
+      resultCont.classList.add("close");
+    }, 300);
+    document.getElementById("cont").classList.remove("wait");
+    document.getElementById("playCont").classList.add("close");
+    document.getElementById("playCont").classList.remove("open");
+    document.getElementById("chooseCont").classList.add("open");
+    document.getElementById("chooseCont").classList.remove("close");
+    setTimeout(() => clearHistory(), 500);
+  };
+
+  useEffect(() => {
+    if (pWin > pLose) {
+      setResult("You won!");
+    } else if (pWin < pLose) {
+      setResult("You lose!");
+    } else {
+      setResult("Draw!");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [final]);
 
   return (
     <div>
       <h2>Singleplayer</h2>
+      <Container id="chooseCont" className="open">
+        <h2>Round</h2>
+        <Form>
+          {["3", "5", "7", "9"].map((label) => (
+            <div key={`radio-${label}`}>
+              <Form.Check
+                inline
+                label={label}
+                name="group1"
+                type="radio"
+                id={`radio${label}`}
+                onChange={() => radioChange(label)}
+              />
+            </div>
+          ))}
+        </Form>
 
-      <Container id="cont">
-        <Row>
-          <Col xs="6">
-            <h2>You</h2>
-            <Button
-              onClick={() => {
-                play(document.getElementById("btnRock"), 0);
-              }}
-              id="btnRock"
-              className="imgBtn"
-              variant="light"
-            >
-              <img className="images" src={Rock}></img>
-            </Button>
-            <Button
-              onClick={() => {
-                play(document.getElementById("btnPaper"), 1);
-              }}
-              id="btnPaper"
-              className="imgBtn"
-              variant="light"
-            >
-              <img className="images" src={Paper}></img>
-            </Button>
-            <Button
-              onClick={() => {
-                play(document.getElementById("btnScissors"), 2);
-              }}
-              id="btnScissors"
-              className="imgBtn"
-              variant="light"
-            >
-              <img className="images" src={Scissors}></img>
-            </Button>
-          </Col>
-          <Col xs="6">
-            <h2>Computer</h2>
-            <Button id="pcBtn0" className="imgBtn" variant="light" disabled>
-              <img className="images" src={Rock}></img>
-            </Button>
-            <Button id="pcBtn1" className="imgBtn" variant="light" disabled>
-              <img className="images" src={Paper}></img>
-            </Button>
-            <Button id="pcBtn2" className="imgBtn" variant="light" disabled>
-              <img className="images" src={Scissors}></img>
-            </Button>
-          </Col>
-        </Row>
+        <Button
+          disabled={disable}
+          id="btnPlay"
+          variant="primary"
+          onClick={() => start()}
+        >
+          Play
+        </Button>
       </Container>
-      <br></br>
-      <br></br>
-      <h4>Score: {score}</h4>
-      <h6>Win: {pWin}</h6>
-      <h6>Draw: {pDraw}</h6>
-      <h6>Lose: {pLose}</h6>
-      <br />
-      <br />
-      <br />
-      <Button variant="secondary" onClick={() => devam()}>
-        devam
-      </Button>
+      <Container id="playCont" className="close">
+        <Container id="cont">
+          <Row>
+            <Col xs="5">
+              <h2>You</h2>
+              <Button
+                onClick={() => {
+                  play(document.getElementById("btnRock"), 0);
+                }}
+                id="btnRock"
+                className="imgBtn"
+                variant="light"
+              >
+                <img className="images" src={Rock}></img>
+              </Button>
+              <Button
+                onClick={() => {
+                  play(document.getElementById("btnPaper"), 1);
+                }}
+                id="btnPaper"
+                className="imgBtn"
+                variant="light"
+              >
+                <img className="images" src={Paper}></img>
+              </Button>
+              <Button
+                onClick={() => {
+                  play(document.getElementById("btnScissors"), 2);
+                }}
+                id="btnScissors"
+                className="imgBtn"
+                variant="light"
+              >
+                <img className="images" src={Scissors}></img>
+              </Button>
+            </Col>
+            <Col xs="2">
+              <h2>
+                {pWin} - {pLose}
+              </h2>
+            </Col>
+            <Col xs="5">
+              <h2>Computer</h2>
+              <Button id="pcBtn0" className="imgBtn" variant="light" disabled>
+                <img className="images" src={Rock}></img>
+              </Button>
+              <Button id="pcBtn1" className="imgBtn" variant="light" disabled>
+                <img className="images" src={Paper}></img>
+              </Button>
+              <Button id="pcBtn2" className="imgBtn" variant="light" disabled>
+                <img className="images" src={Scissors}></img>
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+        <br></br>
+        <br></br>
+        <h6>Win: {pWin}</h6>
+        <h6>Draw: {pDraw}</h6>
+        <h6>Lose: {pLose}</h6>
+      </Container>
+      <Container id="resultCont" className="close">
+        <h2>{result}</h2>
+        <Button variant="primary" onClick={() => playAgain()}>
+          Play Again
+        </Button>{" "}
+        <Link to="/">
+          <Button variant="primary">Main Menu</Button>
+        </Link>
+      </Container>
     </div>
   );
 };
